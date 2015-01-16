17c5f1ac7612f5c5fbbea71dd04ea8edd3425c18
= <theraot@gmail.com>
2015-01-16 03:49:41 -0500

    First test

---
+++ Tests/MapperTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Theraot.Collections.ThreadSafe;
+
+namespace Tests
+{
+    [TestFixture]
+    internal class MapperTests
+    {
+        [Test]
+        public void SetAndGet()
+        {
+            var mapper = new Mapper<int>();
+            const int Input = 42;
+            int result;
+            Assert.IsTrue(mapper.TrySet(0, Input));
+            Assert.IsTrue(mapper.TryGet(0, out result));
+            Assert.AreEqual(Input, result);
+        }
+    }
+}
